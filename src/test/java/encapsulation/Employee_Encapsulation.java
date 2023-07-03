36dd128b5e1d70467434d78b39825a1dc3acfb67
mukesh <mukukmr9@gmail.com>
2023-07-03 13:19:12 +0530

    Updated commit

--- src/test/java/encapsulation/Employee_Encapsulation.java
+++ src/test/java/encapsulation/Employee_Encapsulation.java
@@ -6,18 +6,23 @@ public class Employee_Encapsulation {
 	private String name;
 	private double salary;
 	
+	//Get and Set Emp-id
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	//Get and Set Emp-name
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	//Get and Set Emp-salary
 	public double getSalary() {
 		return salary;
 	}
